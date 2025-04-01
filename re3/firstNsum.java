1ddbd63bae091e2961c9497f9d6c9ed8cd7b4745
Hemant-14942 <hemantinsan6@gmail.com>
2025-04-01 18:32:16 +0530

    completed code in 1st way

--- re3/firstNsum.java
+++ re3/firstNsum.java
@@ -1,5 +1,15 @@
-package re3;
-
+import java.util.*;
 public class firstNsum {
-    
+    public static int sum(int n){
+        if(n<=0){
+            return 0;
+        }
+        return n+sum(n-1);
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int ans =sum(n);
+        System.out.println(ans);
+    }
 }
